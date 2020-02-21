3a37f862dd4d0b0f2efa52376cf3092c4d92ae39
EngelFlores <engel.tads@gmail.com>
2020-02-21 12:07:04 -0300

    Configurações iniciais projeto

---
+++ src/main/java/com/acelerajava/controletransacoes/ControleTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.acelerajava.controletransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acelerajava/controletransacoes/ControleTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acelerajava.controletransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
