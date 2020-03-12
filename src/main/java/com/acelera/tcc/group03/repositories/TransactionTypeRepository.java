b1094e5355186507da48be1cdbeb66a56d7746a7
EngelFlores <engel.flores@thoughtworks.com>
2020-03-12 03:15:58 -0300

    Removing incorrect line

--- src/main/java/com/acelera/tcc/group03/repositories/TransactionTypeRepository.java
+++ src/main/java/com/acelera/tcc/group03/repositories/TransactionTypeRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.acelera.tcc.group03.domains.TransactionType;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
 public interface TransactionTypeRepository extends JpaRepository<TransactionType, Long> {
 }
